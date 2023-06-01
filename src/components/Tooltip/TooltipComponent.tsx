f9ee6db441a22b576c1af7ad2e44dd245feb79f2
Shtukar Artem <shtukar95@mail.ru>
2023-06-01 09:50:00 +0300

    prettier fix

--- src/components/Tooltip/TooltipComponent.tsx
+++ src/components/Tooltip/TooltipComponent.tsx
@@ -1,37 +1,48 @@
 import { ISchedulerOptions } from 'devextreme-react/scheduler';
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 import styles from './styles.module.css';
 
-interface props {
-  data:React.PropsWithChildren<ISchedulerOptions>
+interface Props {
+  data: React.PropsWithChildren<ISchedulerOptions>;
 }
 
-export const TooltipComponent:FC<props> = ({data}: any) => {
-  return (
-    <div className={styles.wrapper}>
-     <div className={styles.fio}> {data.text}</div>
+export const TooltipComponent: FC<Props> = ({ data }: any) => (
+  <div className={styles.wrapper}>
+    <div className={styles.fio}> {data.text}</div>
     <div className={styles.data}>
       <span className={styles.dataTitle}>Крайний срок</span>
-      <span className={styles.dateText}>{data.endDate.getDate()}.{data.endDate.getMonth()}.{data.endDate.getFullYear()} c {data.startDate.getHours()}:{data.startDate.getMinutes()} по {data.endDate.getHours()}:{data.endDate.getMinutes()}</span>
+      <span className={styles.dateText}>
+        {data.endDate.getDate()}.{data.endDate.getMonth()}.
+        {data.endDate.getFullYear()} c {data.startDate.getHours()}:
+        {data.startDate.getMinutes()} по {data.endDate.getHours()}:
+        {data.endDate.getMinutes()}
+      </span>
     </div>
     <div className={styles.clientType}>
-      <span className={styles.dataTitle}>
-      Тип клиента
-      </span>
-      <span className={styles.dateText}>
-      ИП
-      </span>
+      <span className={styles.dataTitle}>Тип клиента</span>
+      <span className={styles.dateText}>ИП</span>
     </div>
-    <div className={styles.email}>
-      {data.email}
+    <div className={styles.email}>{data.email}</div>
+    <div className={styles.number}>{data.number}</div>
+    <div className={styles.footer}>
+      <button
+        type="button"
+        onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+          event.stopPropagation()
+        }
+        className={styles.buttonLight}
+      >
+        Отменить бронь
+      </button>
+      <button
+        type="button"
+        onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+          event.stopPropagation()
+        }
+        className={styles.buttonLight}
+      >
+        Звонить
+      </button>
     </div>
-    <div className={styles.number}>
-      {data.number}
-    </div>
-      <div className={styles.footer}>
-        <button onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => event.stopPropagation()} className={styles.buttonLight}>Отменить бронь</button>
-        <button onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => event.stopPropagation()} className={styles.buttonLight}>Звонить</button>
-      </div>
-    </div>
-  )
-}
+  </div>
+);
