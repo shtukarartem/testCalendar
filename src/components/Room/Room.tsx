77eba76e0dafdc868d7fb77ebc8e74bb26c73eba
YauhenMurachou <emurach1992@gmail.com>
2023-06-02 19:25:33 +0400

    TASK-006 Left column

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,4 +1,7 @@
+import classNames from 'classnames';
 import { FC } from 'react';
+
+import { RoomType } from 'src/types';
 
 import styles from './styles.module.css';
 
@@ -6,16 +9,27 @@ type Props = {
   data: {
     room: string;
     description: string;
+    type: RoomType;
     id: number;
     color: string;
   };
 };
 
+const roomVariants = {
+  workPlace: styles.workPlace,
+  office: styles.office,
+  meeting: styles.meeting,
+};
+
 export const Room: FC<Props> = (props) => {
-  const { room, description } = props.data;
+  const { room, description, type } = props.data;
   return (
     <div>
-      <div className={styles.room}>{room}</div>
+      <div className={styles.room}>
+        <div className={styles.point}></div>
+        <div className={classNames(styles.point, roomVariants[type])}></div>
+        <span>{room}</span>
+      </div>
       <div className={styles.description}>{description}</div>
     </div>
   );
---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type RoomType = 'workPlace' | 'office' | 'meeting';
