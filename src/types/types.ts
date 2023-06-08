b16282b7cd6113a2a6dd0d15abdac901323d3894
YauhenMurachou <emurach1992@gmail.com>
2023-06-08 18:18:08 +0400

    TASK-017 Disable to select booked room

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,14 @@ export type SelectOptions = {
 export type RoomType = 'workPlace' | 'office' | 'meeting';
 
 export type RentStatusType = 'paid' | 'booked' | 'overdue';
+
+export type BookingType = {
+  text: string;
+  ownerId: number[];
+  startDate: Date;
+  endDate: Date;
+  rooms: number;
+  email: string;
+  number: string;
+  status: RentStatusType;
+};
